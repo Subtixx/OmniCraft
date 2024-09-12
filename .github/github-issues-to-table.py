eba43d2a867da01696ac6552a93d9a13da0a73ea
Dominic Hock <d.hock@it-hock.de>
2024-09-12 18:27:05 +0200

    chore: update issues in README

--- .github/github-issues-to-table.py
+++ .github/github-issues-to-table.py
@@ -17,6 +17,18 @@ payload = {
         ... on ProjectV2 {
           items(first: 100) {
             nodes {
+              fieldValues(first: 6) {
+                nodes {
+                  ... on ProjectV2ItemFieldSingleSelectValue {
+                    name
+                    field {
+                      ... on ProjectV2FieldCommon {
+                        name
+                      }
+                    }
+                  }
+                }
+              }
               id
               content {
                 ... on DraftIssue {
@@ -28,6 +40,7 @@ payload = {
                   url
                   state
                   createdAt
+                  updatedAt
                   milestone {
                     title
                     url
@@ -43,9 +56,14 @@ payload = {
 }
 
 def sort_issue(issue):
-    sort = 0
-    if issue["status"] == "OPEN":
+    sort = -1
+    if issue["status"] == "Done":
+        sort = 0
+    elif issue["status"] == "In progress":
         sort = 1
+    elif issue["status"] == "In review":
+        sort = 2
+
     if issue["milestone"] == "None":
         return sort + 1000000000
     milestones = issue["milestone"].split(".")
@@ -58,25 +76,35 @@ data = data["data"]["node"]["items"]["nodes"]
 issues = []
 # content title
 for item in data:
-    print(item)
+    fieldValues = item["fieldValues"]["nodes"]
+    status = "UNKNOWN"
+    for fieldValue in fieldValues:
+        if "field" not in fieldValue:
+            continue
+        if fieldValue["field"]["name"] == "Status":
+            status = fieldValue["name"]
+            break
     title = item["content"]["title"]
     url = ""
-    status = "OPEN"
+    state = "OPEN"
     created_at = item["content"]["createdAt"]
+    updated_at = item["content"]["updatedAt"]
     milestone = "None"
     milestone_url = ""
     if "url" in item["content"]:
         url = item["content"]["url"]
     if "state" in item["content"] and item["content"]["state"] is not None:
-        status = item["content"]["state"]
+        state = item["content"]["state"]
     if "milestone" in item["content"] and item["content"]["milestone"] is not None:
         milestone = item["content"]["milestone"]["title"]
         milestone_url = item["content"]["milestone"]["url"]
     issues.append({
         "title": title,
         "status": status,
+        "state": state,
         "url": url,
         "created_at": created_at,
+        "updated_at": updated_at,
         "milestone": milestone,
         "milestone_url": milestone_url
     })
@@ -84,24 +112,29 @@ for item in data:
 issues.sort(key=sort_issue)
 
 with open("issue_list.md", "w") as f:
-    f.write("| Title | Status | Milestone | Created |\n")
-    f.write("| --- | --- | --- | --- |\n")
+    f.write("| Title | Status | Milestone | Created | Updated |\n")
+    f.write("| --- | --- | --- | --- | --- |\n")
     print("Found " + str(len(data)) + " issues")
     for issue in issues:
         status = "**_~~Done~~_**"
-        if issue["status"] == "OPEN":
-            status = "Planned"
+        if issue["status"] != "Done":
+            status = issue["status"]
         url = issue["url"]
         title = issue["title"]
         created_at = issue["created_at"]
+        updated_at = issue["updated_at"]
         milestone = issue["milestone"]
         milestone_url = issue["milestone_url"]
         # Parse date
-        created = datetime.datetime.fromisoformat(created_at).strftime("%d/%m/%Y")
+        created = datetime.datetime.fromisoformat(created_at).strftime("%d/%m/%Y %H:%M")
+        updated = datetime.datetime.fromisoformat(updated_at).strftime("%d/%m/%Y %H:%M")
+        # Check if created and updated are the same
+        if created_at == updated_at:
+            updated = "Never"
 
         f.write(f"| [{title}]({url}) | {status} |")
         if milestone == "None":
             f.write(" None |")
         else:
             f.write(f" [{milestone}]({milestone_url}) |")
-        f.write(f" {created} |\n")
+        f.write(f" {created} | {updated} |\n")
